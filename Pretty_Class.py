df9e621cd7cc5544da17e7ffdb54667ff3bc2c6f
Vishal <vishal.singh@nextuple.com>
2026-02-18 19:48:12 +0530

    added OOP coffe machine project

---
+++ Pretty_Class.py
@@ -0,0 +1,15 @@
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+#table.align = "l"
+#table.align="r"
+
+print(table)
+print(table.align)
---
+++ Turtle_Class.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("coral")
+
+tim.forward(100)
+
+screen = Screen()
+screen.exitonclick()
+
+
+
