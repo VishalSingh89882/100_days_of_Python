56555dff7a75dcffe9b7208d5902d3636a400b7d
Vishal <vishal.singh@nextuple.com>
2026-02-21 11:39:33 +0530

    added turtle race project

---
+++ hirst-painting-start/Day-19/main.py
@@ -0,0 +1,35 @@
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forward():
+    tim.forward(10)
+
+def move_backward():
+    tim.backward(10)
+
+def move_clockwise():
+    tim.right(10)
+
+def move_anticlockwise():
+    tim.left(10)
+
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(key= "w",fun = move_forward)
+screen.onkey(key= "s",fun = move_backward)
+screen.onkey(key= "d",fun = move_clockwise)
+screen.onkey(key= "a",fun = move_anticlockwise)
+screen.onkey(key= "c",fun = clear_screen)
+
+
+screen.exitonclick()
