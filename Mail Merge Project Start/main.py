3b573023e34e12439086bb0a240b0ff94f81263c
Vishal <vishal.singh@nextuple.com>
2026-02-24 22:48:24 +0530

    added mail merge project

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -14,5 +14,5 @@ with open("Mail Merge Project Start/Input/Names/invited_names.txt") as names_fil
         with open("Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter_file:
             letter_contents = letter_file.read()
             new_letter = letter_contents.replace("[name]", stripped_name)
-            with open(f"Mail Merge Project Start/Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            with open(f"Mail Merge Project Start/Output/ReadyToSend/letter_for_{stripped_name}.docx", mode="w") as completed_letter:
                 completed_letter.write(new_letter)
